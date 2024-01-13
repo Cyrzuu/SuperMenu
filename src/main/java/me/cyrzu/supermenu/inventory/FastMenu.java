4b83350478f88245162c725b543e6f87fec160cc
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-01-14 00:31:05 +0100

    1.1.4

---
+++ src/main/java/me/cyrzu/supermenu/inventory/FastMenu.java
@@ -0,0 +1,89 @@
+package me.cyrzu.supermenu.inventory;
+
+import me.cyrzu.supermenu.button.ButtonHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+public class FastMenu extends MenuHandler {
+
+    public FastMenu(int rows, String title) {
+        super(rows, title);
+    }
+
+    public FastMenu(int rows) {
+        super(rows);
+    }
+
+    @Override
+    public FastMenu setButton(int slot, ButtonHandler button) {
+        super.setButton(slot, button);
+        return this;
+    }
+
+    public FastMenu onClose(BiConsumer<@NotNull Player, @NotNull Inventory> action) {
+        super.onClose(action);
+        return this;
+    }
+
+    public FastMenu setItem(int slot, @NotNull ItemStack stack) {
+        super.setItem(slot, stack);
+        return this;
+    }
+
+    public FastMenu setMoveableSlot(int slot) {
+        return setMoveableSlot(slot, null, null);
+    }
+
+    public FastMenu setMoveableSlot(int slot, @Nullable BiFunction<Player, ItemStack, Boolean> put) {
+        return setMoveableSlot(slot, put, null);
+    }
+
+    public FastMenu setMoveableSlot(int slot, @Nullable BiFunction<Player, ItemStack, Boolean> put,
+                                       @Nullable BiFunction<Player, ItemStack, Boolean> take) {
+        super.setMoveableSlot(slot, put, take);
+        return this;
+    }
+
+    @Override
+    public FastMenu setTask(long period, @NotNull Runnable runnable) {
+        super.setTask(period, runnable);
+        return this;
+    }
+
+    @Override
+    public FastMenu setTask(@NotNull Runnable runnable) {
+        super.setTask(runnable);
+        return this;
+    }
+
+
+    public FastMenu setPermission(@NotNull String permission) {
+        super.setPermission(new Permission(permission));
+        return this;
+    }
+
+    @Override
+    public FastMenu setUnregisterOnClose(boolean unregister) {
+        super.setUnregisterOnClose(unregister);
+        return this;
+    }
+
+    @Override
+    public FastMenu fillSlots(@NotNull ItemStack stack) {
+        return fillSlots(stack, new Integer[0]);
+    }
+
+    @Override
+    public FastMenu fillSlots(@NotNull ItemStack stack, @NotNull Integer... ignore) {
+        super.fillSlots(stack, ignore);
+        return this;
+    }
+
+}
---
+++ src/main/java/me/cyrzu/supermenu/inventory/MenuHandler.java
@@ -0,0 +1,231 @@
+package me.cyrzu.supermenu.inventory;
+
+import lombok.Getter;
+import me.cyrzu.supermenu.MenuManager;
+import me.cyrzu.supermenu.MenuMoveableSlot;
+import me.cyrzu.supermenu.MenuTask;
+import me.cyrzu.supermenu.button.ButtonHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+
+public abstract class MenuHandler {
+
+    private final MenuManager menuManager;
+
+    protected final @NotNull Inventory inventory;
+
+    protected final @NotNull Map<Integer, ButtonHandler> buttons;
+
+    private final @NotNull Map<Integer, MenuMoveableSlot> moveableSlots;
+
+    private @Nullable BiConsumer<@NotNull Player, @NotNull Inventory> close;
+
+    protected @Nullable MenuTask menuTask = null;
+
+    protected @Nullable Permission permission = null;
+
+    private final @NotNull Map<UUID, @NotNull Long> cooldown;
+
+    @Getter
+    private boolean unregisterOnClose = true;
+
+    public MenuHandler(int rows) {
+        this(rows, "");
+    }
+
+    public MenuHandler(int rows, String title) {
+        this.menuManager = MenuManager.getManager();
+        this.inventory = Bukkit.createInventory(null, Math.min(6, rows) * 9, title);
+        this.buttons = new HashMap<>();
+        this.moveableSlots = new HashMap<>();
+        this.cooldown = new HashMap<>();
+    }
+
+    public final void onClick(@NotNull Player player, @NotNull InventoryClickEvent event) {
+        ButtonHandler button = buttons.get(event.getRawSlot());
+        if(button != null) button.runClick(player, event);
+    }
+
+    public MenuHandler onClose(@NotNull BiConsumer<@NotNull Player, @NotNull Inventory> action) {
+        this.close = action;
+        return this;
+    }
+
+    public MenuHandler setButton(int slot, ButtonHandler buttonHandler) {
+        int min = Math.min(inventory.getSize() - 1, slot);
+        inventory.setItem(min, buttonHandler.getStack());
+        buttons.put(min, buttonHandler);
+        return this;
+    }
+
+    public MenuHandler setItem(int slot, @NotNull ItemStack stack) {
+        inventory.setItem(Math.min(inventory.getSize() - 1, slot), stack);
+        return this;
+    }
+
+    public MenuHandler setMoveableSlot(int slot) {
+        return setMoveableSlot(slot, null, null);
+    }
+
+    public MenuHandler setMoveableSlot(int slot, @Nullable BiFunction<Player, ItemStack, Boolean> put) {
+        return setMoveableSlot(slot, put, null);
+    }
+
+    public MenuHandler setMoveableSlot(int slot, @Nullable BiFunction<Player, ItemStack, Boolean> put,
+                                       @Nullable BiFunction<Player, ItemStack, Boolean> take) {
+        moveableSlots.put(slot, new MenuMoveableSlot(put, take));
+        return this;
+    }
+
+    public MenuHandler setTask(long period, @NotNull Runnable runnable) {
+        this.menuTask = new MenuTask(menuManager.getInstance(), period, runnable);
+        return this;
+    }
+
+    public MenuHandler setTask(@NotNull Runnable runnable) {
+        this.menuTask = new MenuTask(menuManager.getInstance(), runnable);
+        return this;
+    }
+    public MenuHandler setPermission(@NotNull Permission permission) {
+        this.permission = permission;
+        return this;
+    }
+
+    public MenuHandler setUnregisterOnClose(boolean unregister) {
+        this.unregisterOnClose = unregister;
+        return this;
+    }
+
+    public final boolean hasPermission(@NotNull HumanEntity player) {
+        return permission == null || player.hasPermission(permission);
+    }
+
+    public final boolean hasPermission(@NotNull Player player) {
+        return permission == null || player.hasPermission(permission);
+    }
+
+    public final void cancelTask() {
+        if (menuTask == null) {
+            return;
+        }
+
+        menuTask.cancel();
+    }
+
+    public final @Nullable MenuMoveableSlot getMoveableSlot(int slot) {
+        return moveableSlots.get(slot);
+    }
+
+    public final @Nullable ButtonHandler getButton(int slot) {
+        return buttons.get(slot);
+    }
+
+    public final MenuHandler start() {
+        MenuManager.registerMenu(this);
+
+        if(menuTask != null) {
+            menuTask.run();
+        }
+
+        return this;
+    }
+
+    public final void open(@NotNull Player player) {
+        player.openInventory(inventory);
+    }
+
+    public final void open(Player... players) {
+        Arrays.stream(players).filter(Objects::nonNull).forEach(this::open);
+    }
+
+    public final void open(Collection<Player> players) {
+        players.stream().filter(Objects::nonNull).forEach(this::open);
+    }
+
+    public final @NotNull Inventory getInventory() {
+        return inventory;
+    }
+
+    public boolean hasMoveableSlots() {
+        return !moveableSlots.isEmpty();
+    }
+
+    public boolean hasCooldown(@NotNull Player player, boolean put) {
+        UUID uuid = player.getUniqueId();
+        long cooldown = this.cooldown.getOrDefault(uuid, -1L);
+        long time = System.currentTimeMillis();
+
+        if (cooldown != -1L && time < cooldown) {
+            return true;
+        } else {
+            if(put) {
+                this.cooldown.put(uuid, System.currentTimeMillis() + 150L);
+            }
+
+            return false;
+        }
+    }
+
+    public final void onClose(@NotNull Player player) {
+        if(close != null) {
+            close.accept(player, this.inventory);
+        }
+    }
+
+    public int randomSlot() {
+        Random random = new Random();
+        return random.nextInt(inventory.getSize());
+    }
+
+    public MenuHandler fillSlots(@NotNull ItemStack stack) {
+        return fillSlots(stack, new Integer[0]);
+    }
+
+    public MenuHandler fillSlots(@NotNull ItemStack stack, @NotNull Integer... ignore) {
+        Set<@NotNull Integer> collect = Arrays.stream(ignore).collect(Collectors.toSet());
+
+        for (int i = 0; i < inventory.getSize(); i++) {
+            if(collect.contains(i)) {
+                continue;
+            }
+
+            inventory.setItem(i, stack);
+        }
+
+        return this;
+    }
+
+    public void fillBorder(@NotNull ItemStack stack) {
+        int rows = inventory.getSize() / 9;
+        if (rows <= 2) {
+            return;
+        }
+
+        for (int i = 0; i < rows * 9; i++) {
+            if (i <= 8 || (i >= rows * 9 - 8 && i <= rows * 9 - 2) || i % 9 == 8 || i % 9 == 0)
+                inventory.setItem(i, stack);
+        }
+    }
+
+    public @Nullable ItemStack getStack(int slot) {
+        return inventory.getItem(slot);
+    }
+
+    public void unregister() {
+        MenuManager.getManager().unregister(this);
+    }
+
+}
